6c3d4485c682f70cf2876de1eba5df3162968457
JanezRadescek <janez.radescek@gmail.com>
2017-12-21 15:18:21 +0100

    save

--- src/cli/CliTest.java
+++ src/cli/CliTest.java
@@ -8,7 +8,18 @@ public class CliTest {
 
 	@Test
 	public void testMain() {
-		fail("Not yet implemented");
+		String[] what = new String[]{"-s","-c","-r","-b"};
+		String[] options = new String[]{"-i"};
+		String inputDir = "C:\\Users\\janez\\workspace\\S2_rw\\IO\\Original";
+		String[] inFiles = new String[]{"test2.s2"};
+		String outDir = "C:\\Users\\janez\\workspace\\S2_rw\\UnitTests";
+		String[] outFiles = new String[]{"Junit.txt"};
+		
+
+		
+		String[] args = new String[]{what[0],options[0],inputDir,inFiles[0],outDir,outFiles[0]};
+		Cli.main(args);
+		//fail("Not yet implemented");
 	}
 
 }
