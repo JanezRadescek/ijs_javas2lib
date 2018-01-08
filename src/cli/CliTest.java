ac22397104a1ad101cc005ace51549beee9d2de9
JanezRadescek <janez.radescek@gmail.com>
2018-01-08 11:37:41 +0100

    save
    dodelan unit test

--- src/cli/CliTest.java
+++ src/cli/CliTest.java
@@ -1,14 +1,16 @@
 package cli;
 
 import org.apache.commons.io.FileUtils;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import org.junit.runners.Parameterized;
 import org.junit.runner.RunWith;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 @RunWith(Parameterized.class)
@@ -74,42 +76,116 @@ public class CliTest {
 	@Test
 	public void outS2Test()
 	{
+		
+		checkCopy();
+		//first 3 lines are struct definitions
+		assertEquals(3+1, checkSingleton(17L));
+		assertEquals(3+3, checkSingleton(16L));
+		assertEquals(3+0, checkSingleton(15L));
+		
+
+	}
+	
+
+	private void checkCopy() {
+		
 		//do copy of original
-		Cli.main(new String[]{"-c", "-i", inDir, inFile, "-o", outDir, "KopijaOriginala.s2"});
+				Cli.main(new String[]{"-c", "-i", inDir, inFile, "-o", outDir, "KopijaOriginala.s2"});
 
-		//read copy
-		Cli.main(new String[]{"-r", "-i", outDir, "KopijaOriginala.s2",
-				"-o", outDir, "IzpisKopije.csv"});
+				//read copy
+				Cli.main(new String[]{"-r", "-i", outDir, "KopijaOriginala.s2",
+						"-o", outDir, "IzpisKopije.csv"});
 
-		//compare copy.csv with corect.csv
-		File corect = new File(inDir + File.separator + "generated.csv");
-		File testing = new File(outDir + File.separator + "IzpisKopije.csv");
-		boolean isTwoEqual = false;
+				//compare copy.csv with corect.csv
+				File corect = new File(inDir + File.separator + "generated.csv");
+				File testing = new File(outDir + File.separator + "IzpisKopije.csv");
+				boolean isTwoEqual = false;
+				try {
+					isTwoEqual = FileUtils.contentEquals(corect, testing);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				assertEquals(true, isTwoEqual);
+	}
+
+	
+	/**
+	 * For checking behavior of time interval
+	 * @param cutTime
+	 * @return number of lines in CSV of cutted file
+	 */
+	public int checkSingleton(long cutTime)
+	{
+		String nameS2 = "Singleton" + cutTime +".s2";
+		String nameCSV = "Singleton" + cutTime +".csv";
+		Cli.main(new String[]{"-c", "-i", inDir, inFile, "-o", outDir, nameS2,
+				"-t", Double.toString(cutTime*1E-9),Double.toString((cutTime+1)*1E-9)});
+		Cli.main(new String[]{"-r", "-i", outDir, nameS2, "-o", outDir, nameCSV});
+		
+		int lines = 0;
 		try {
-			isTwoEqual = FileUtils.contentEquals(corect, testing);
-
+			BufferedReader bf = new BufferedReader(new FileReader(outDir + File.separator + nameCSV));
+			String line;
+			while((line = bf.readLine()) != null)
+			{
+				lines++;
+			}
+			bf.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		assertEquals(true, isTwoEqual);
-
+		
+		return lines;
 	}
 
 	@Test
 	public void buildTest()
 	{
 		
-		searcANDdestroy(0L); //additive identity of field
-		searcANDdestroy(2L); //before first data
-		searcANDdestroy(5L); //on first data
-		searcANDdestroy(10L);//betwen data
-		searcANDdestroy(17L);//on data
-		searcANDdestroy(19L);//on last data
-		searcANDdestroy(20L);//after data
+		devideANDconcor(0L); //additive identity of field
+		devideANDconcor(2L); //before first data
+		devideANDconcor(5L); //on first data
+		devideANDconcor(10L);//betwen data
+		devideANDconcor(17L);//on data
+		devideANDconcor(19L);//on last data
+		devideANDconcor(20L);//after data
+		
+		cutMiddle(6L,17L);
 
 	}
 
-	private void searcANDdestroy(Long cutTime)
+	private void cutMiddle(long head, long tail) {
+		Cli.main(new String[]{"-c", "-i", inDir, inFile, "-t", "0", Double.toString(head*1E-9),
+				"-o", outDir, "head"+head+".s2"});
+		
+		Cli.main(new String[]{"-c", "-i", inDir, inFile, "-t", Double.toString(tail*1E-9), "1",
+				"-o", outDir, "tail"+tail+".s2"});
+		
+		Cli.main(new String[]{"-m", "true", "-i", outDir, "head"+head+".s2",
+				"-v", outDir, "tail"+tail+".s2",
+				"-o", outDir, "cutedMiddle.s2"});
+		
+		Cli.main(new String[]{"-r", "-i", outDir, "cutedMiddle.s2",
+				"-o", outDir, "cutedMiddle.csv"});
+		
+		File corect = new File(inDir + File.separator + "cutedMiddle.csv");
+		File testing = new File(outDir + File.separator + "cutedMiddle.csv");
+
+		
+		try {
+			boolean isTwoEqual = FileUtils.contentEquals(corect, testing);
+			assertEquals("Cuts out middle corectly " ,true, isTwoEqual);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/** Cut original file at cutTime and reconstruct it again
+	 * @param cutTime
+	 */
+	private void devideANDconcor(Long cutTime)
 	{
 		//cut S2 at 
 		Cli.main(new String[]{"-c", "-i", inDir, inFile, "-t", "0", Double.toString(cutTime*1E-9),
