5adfaf9ac720baa903a9d53f5665928fb5f13786
JanezRadescek <janez.radescek@gmail.com>
2018-01-17 15:01:30 +0100

    design update

--- src/gui/BaurOkno2.java
+++ src/gui/BaurOkno2.java
@@ -21,18 +21,14 @@ import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JLabel;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 
 public class BaurOkno2 extends JFrame {
+
 
 	/**
 	 * 
@@ -41,14 +37,18 @@ public class BaurOkno2 extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtCliArgs;
 	JFileChooser chooser = new JFileChooser();
-	
+
 	private HashMap<String,ArrayList<String>> cliArgs = new HashMap<String,ArrayList<String>>();
-	private JTextField textField_DMainInput;
-	private JTextField textField_DSecondaryInput;
-	private JTextField textField_DOutput;
-	private JTextField txtlongmaxvalue;
-	private JTextField txtall;
-	private JTextField txtall_1;
+	private HashMap<String,String[]> dArguments = new HashMap<String,String[]>();
+	HashMap<String,JComponent[]> components;
+
+	private JTextField textField_MainInput;
+	private JTextField textField_SecondaryInput;
+	private JTextField textField_OutputDire;
+	private JTextField txtHandles;
+	private JTextField textField_OutputName;
+	private JTextField textField_start;
+	private JTextField textField_end;
 
 	/**
 	 * Launch the application.
@@ -76,117 +76,276 @@ public class BaurOkno2 extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//chooser
 		chooser.setCurrentDirectory(new File("."));
-		
-		//Menu
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnNewMenu = new JMenu("file");
-		menuBar.add(mnNewMenu);
-		
-		JMenuItem mntmSave = new JMenuItem("save");
-		mnNewMenu.add(mntmSave);
-		
+
+		//default falues
+		{
+			dArguments.put("time",new String[]{"0 " + (60*60)});
+			dArguments.put("handles", new String[]{Long.toString(Long.MAX_VALUE)});
+			dArguments.put("handles", new String[]{Long.toString(Long.MAX_VALUE)});
+		}
+
+
+
+		//MenuBar
+		{
+			JMenuBar menuBar = new JMenuBar();
+			setJMenuBar(menuBar);
+
+			JMenu mnNewMenu = new JMenu("file");
+			menuBar.add(mnNewMenu);
+
+			JMenuItem mntmSave = new JMenuItem("save");
+			mnNewMenu.add(mntmSave);
+
+			JMenu mnHelp = new JMenu("Help");
+			menuBar.add(mnHelp);
+
+			JMenuItem mntmDocumentation = new JMenuItem("Documentation");
+			mnHelp.add(mntmDocumentation);
+		}
 		//Panel
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setBackground(Color.red);
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_radioButtons = new JPanel();
-		panel_radioButtons.setBackground(Color.GREEN);
-		panel_radioButtons.setForeground(Color.BLACK);
-		contentPane.add(panel_radioButtons, BorderLayout.WEST);
-		
-		ButtonGroup JRadioGRoup = new ButtonGroup();
-		panel_radioButtons.setLayout(new MigLayout("", "[71px]", "[51px][51px][51px][51px]"));
-		
-		JRadioButton rdbtn1 = new JRadioButton("Statistics");
-		panel_radioButtons.add(rdbtn1, "cell 0 0,grow");
-		JRadioGRoup.add(rdbtn1);
-		
-		JRadioButton rdbtn2 = new JRadioButton("CSV");
-		panel_radioButtons.add(rdbtn2, "cell 0 1,grow");
-		JRadioGRoup.add(rdbtn2); 
-		
-		JRadioButton rdbtn3 = new JRadioButton("cut S2");
-		panel_radioButtons.add(rdbtn3, "cell 0 2,grow");
-		JRadioGRoup.add(rdbtn3); 
-		
-		JRadioButton rdbtn4 = new JRadioButton("Merge S2");
-		JRadioGRoup.add(rdbtn4);
-		panel_radioButtons.add(rdbtn4, "cell 0 3,grow");
-		
-		JSplitPane splitPane_South = new JSplitPane();
-		splitPane_South.setResizeWeight(1);
-		contentPane.add(splitPane_South, BorderLayout.SOUTH);
-		
-		txtCliArgs = new JTextField();
-		txtCliArgs.setEditable(false);
-		splitPane_South.setLeftComponent(txtCliArgs);
-		txtCliArgs.setText("Cli Args");
-		
-		JButton btnNewButton = new JButton("GO");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		{
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			contentPane.setBackground(Color.red);
+			setContentPane(contentPane);
+			contentPane.setLayout(new BorderLayout(0, 0));
+		}
+
+
+
+		//arguments + go
+		{
+			JSplitPane splitPane_South = new JSplitPane();
+			splitPane_South.setResizeWeight(1);
+			contentPane.add(splitPane_South, BorderLayout.SOUTH);
+
+			txtCliArgs = new JTextField();
+			txtCliArgs.setEditable(false);
+			splitPane_South.setLeftComponent(txtCliArgs);
+			txtCliArgs.setText("Cli Args");
+
+			JButton btnNewButton = new JButton("GO");
+			btnNewButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+				}
+			});
+			splitPane_South.setRightComponent(btnNewButton);
+		}
+
+
+		//Options
+		{
+			JPanel panel_Options = new JPanel();
+			panel_Options.setBackground(Color.CYAN);
+			contentPane.add(panel_Options, BorderLayout.CENTER);
+
+			components = new HashMap<String,JComponent[]>();
+			panel_Options.setLayout(new MigLayout("", "[131px][131px,grow]", "[29px][29px][29px][29px][29px,grow][29px][29px]"));
+
+			{
+				JButton btn_MainInput = new JButton("Main input");
+				btn_MainInput.setEnabled(false);
+				panel_Options.add(btn_MainInput, "cell 0 0,grow");
+				//components.add(btn_MainInput);
+
+				textField_MainInput = new JTextField();
+				textField_MainInput.setEnabled(false);
+				panel_Options.add(textField_MainInput, "cell 1 0,grow");
+				textField_MainInput.setColumns(10);
+				
+				components.put("in1",new JComponent[]{btn_MainInput,textField_MainInput});
 			}
-		});
-		splitPane_South.setRightComponent(btnNewButton);
-		
-		JPanel panel_Options = new JPanel();
-		panel_Options.setBackground(Color.CYAN);
-		contentPane.add(panel_Options, BorderLayout.CENTER);
-		panel_Options.setLayout(new MigLayout("", "[176px][176px]", "[34px][34px][34px][34px][34px][34px]"));
-		
-		JButton btn_MainInput = new JButton("Main input");
-		panel_Options.add(btn_MainInput, "cell 0 0,grow");
-		
-		textField_DMainInput = new JTextField();
-		panel_Options.add(textField_DMainInput, "cell 1 0,grow");
-		textField_DMainInput.setColumns(10);
-		
-		JButton btn_SecondaryInput = new JButton("Secondary input");
-		panel_Options.add(btn_SecondaryInput, "cell 0 1,grow");
-		
-		textField_DSecondaryInput = new JTextField();
-		panel_Options.add(textField_DSecondaryInput, "cell 1 1,grow");
-		textField_DSecondaryInput.setColumns(10);
-		
-		JButton btnNewButton_1 = new JButton("Output");
-		panel_Options.add(btnNewButton_1, "cell 0 2,grow");
-		
-		textField_DOutput = new JTextField();
-		panel_Options.add(textField_DOutput, "cell 1 2,grow");
-		textField_DOutput.setColumns(10);
-		
-		JButton btn_Time = new JButton("Time interval");
-		panel_Options.add(btn_Time, "cell 0 3,grow");
-		
-		txtlongmaxvalue = new JTextField();
-		txtlongmaxvalue.setText("0,Long.MAXVALUE");
-		panel_Options.add(txtlongmaxvalue, "cell 1 3,grow");
-		txtlongmaxvalue.setColumns(10);
-		
-		JButton btnNewButton_2 = new JButton("Handle");
-		panel_Options.add(btnNewButton_2, "cell 0 4,grow");
-		
-		JButton btn_DataTypes = new JButton("Data types");
-		btn_DataTypes.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+			{
+				JButton btn_SecondaryInput = new JButton("Secondary input");
+				btn_SecondaryInput.setEnabled(false);
+				panel_Options.add(btn_SecondaryInput, "cell 0 1,grow");
+				//components.add(btn_SecondaryInput);
+
+				textField_SecondaryInput = new JTextField();
+				textField_SecondaryInput.setEnabled(false);
+				panel_Options.add(textField_SecondaryInput, "cell 1 1,grow");
+				textField_SecondaryInput.setColumns(10);
+				
+				components.put("in2",new JComponent[]{btn_SecondaryInput,textField_SecondaryInput});
 			}
-		});
-		
-		txtall = new JTextField();
-		txtall.setText("\"All\" , 32 in binary");
-		panel_Options.add(txtall, "cell 1 4,grow");
-		txtall.setColumns(10);
-		panel_Options.add(btn_DataTypes, "cell 0 5,grow");
-		
-		txtall_1 = new JTextField();
-		txtall_1.setText("\"All\" 1111111111");
-		panel_Options.add(txtall_1, "cell 1 5,grow");
-		txtall_1.setColumns(10);
-		
+			
+			{
+				JButton btn_Output = new JButton("Output directory");
+				btn_Output.setEnabled(false);
+				panel_Options.add(btn_Output, "cell 0 2,grow");
+				//components.add(btn_Output);
+
+				textField_OutputDire = new JTextField();
+				textField_OutputDire.setEnabled(false);
+				panel_Options.add(textField_OutputDire, "cell 1 2,grow");
+				textField_OutputDire.setColumns(10);
+				
+				//name
+			
+				JButton btn_Name = new JButton("Output file name");
+				btn_Name.setEnabled(false);
+				panel_Options.add(btn_Name, "cell 0 3,grow");
+				//components.add(btn_Time);
+
+				textField_OutputName = new JTextField();
+				textField_OutputName.setEnabled(false);
+				panel_Options.add(textField_OutputName, "cell 1 3,grow");
+				textField_OutputName.setColumns(10);
+				
+				components.put("out",new JComponent[]{btn_Output,textField_OutputDire,btn_Name,textField_OutputName});
+			}
+
+			{
+				JButton btn_Time = new JButton("Time interval");
+				btn_Time.setEnabled(false);
+				panel_Options.add(btn_Time, "cell 0 4,grow");
+
+				JSplitPane splitPane = new JSplitPane();
+				splitPane.setResizeWeight(0.5);
+				panel_Options.add(splitPane, "cell 1 4,grow");
+
+				textField_start = new JTextField();
+				textField_start.setEnabled(false);
+				splitPane.setLeftComponent(textField_start);
+				textField_start.setColumns(10);
+
+				textField_end = new JTextField();
+				textField_end.setEnabled(false);
+				splitPane.setRightComponent(textField_end);
+				textField_end.setColumns(10);
+				
+				components.put("time",new JComponent[]{btn_Time,textField_start,textField_end});
+			}
+
+			{
+				JButton btn_Handles = new JButton("Handles");
+				btn_Handles.setEnabled(false);
+				panel_Options.add(btn_Handles, "cell 0 5,grow");
+				//components.add(btn_Handles);
+
+				JPanel panel_1 = new JPanel();
+				panel_Options.add(panel_1, "cell 1 5,grow");
+				panel_1.setLayout(new BorderLayout(0, 0));
+
+				JCheckBox chckbxAll = new JCheckBox("All");
+				chckbxAll.setEnabled(false);
+				chckbxAll.setSelected(true);
+				panel_1.add(chckbxAll, BorderLayout.WEST);
+				//components.add(chckbxAll);
+
+				txtHandles = new JTextField();
+				txtHandles.setEnabled(false);
+				txtHandles.setText("handles");
+				panel_1.add(txtHandles, BorderLayout.CENTER);
+				txtHandles.setColumns(10);
+				
+				components.put("handles",new JComponent[]{btn_Handles,chckbxAll,txtHandles});
+			}
+
+			{
+				JButton btn_DataTypes = new JButton("Data types");
+				btn_DataTypes.setEnabled(false);
+				panel_Options.add(btn_DataTypes, "cell 0 6,grow");
+				//components.add(btn_DataTypes);
+
+				JPanel panel = new JPanel();
+				panel_Options.add(panel, "cell 1 6,grow");
+				panel.setLayout(new BorderLayout(0, 0));
+
+				JCheckBox chckbx_C = new JCheckBox("Comments");
+				chckbx_C.setEnabled(false);
+				chckbx_C.setSelected(true);
+				panel.add(chckbx_C, BorderLayout.NORTH);
+				//components.add(chckbx_C);
+
+				JCheckBox chckbx_SM = new JCheckBox("Special messeges");
+				chckbx_SM.setEnabled(false);
+				chckbx_SM.setSelected(true);
+				panel.add(chckbx_SM, BorderLayout.CENTER);
+				//components.add(chckbx_SM);
+
+				JCheckBox chckbx_MD = new JCheckBox("Meta data");
+				chckbx_MD.setEnabled(false);
+				chckbx_MD.setSelected(true);
+				panel.add(chckbx_MD, BorderLayout.SOUTH);
+				components.put("data",new JComponent[]{btn_DataTypes,chckbx_C,chckbx_SM,chckbx_MD});
+			}
+		}
+
+		//Radio Panel
+		{
+			JPanel panel_radioButtons = new JPanel();
+			panel_radioButtons.setBackground(Color.GREEN);
+			panel_radioButtons.setForeground(Color.BLACK);
+			contentPane.add(panel_radioButtons, BorderLayout.WEST);
+
+			ButtonGroup JRadioGRoup = new ButtonGroup();
+			panel_radioButtons.setLayout(new MigLayout("", "[71px]", "[51px][51px][51px][51px]"));
+
+			JRadioButton rdbtn1 = new JRadioButton("Statistics");
+			rdbtn1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					enableButtons(new String[]{"in1","out"});
+				}
+			});
+			panel_radioButtons.add(rdbtn1, "cell 0 0,grow");
+			JRadioGRoup.add(rdbtn1);
+
+			JRadioButton rdbtn2 = new JRadioButton("CSV");
+			rdbtn2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					enableButtons(new String[]{"in1","out","time","handles"});
+				}
+			});
+			panel_radioButtons.add(rdbtn2, "cell 0 1,grow");
+			JRadioGRoup.add(rdbtn2); 
+
+			JRadioButton rdbtn3 = new JRadioButton("cut S2");
+			rdbtn3.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					enableButtons(new String[]{"in1","out","time","handles","data"});
+				}
+			});
+			panel_radioButtons.add(rdbtn3, "cell 0 2,grow");
+			JRadioGRoup.add(rdbtn3); 
+
+			JRadioButton rdbtn4 = new JRadioButton("Merge S2 - New handles");
+			rdbtn4.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					enableButtons(new String[]{"in1","in2","out"});
+				}
+			});
+			JRadioGRoup.add(rdbtn4);
+			panel_radioButtons.add(rdbtn4, "cell 0 3,grow");
+
+			JRadioButton rdbtn5 = new JRadioButton("Merge S2 - Same handles");
+			rdbtn5.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					enableButtons(new String[]{"in1","in2","out"});
+				}
+			});
+			JRadioGRoup.add(rdbtn5);
+			panel_radioButtons.add(rdbtn5, "cell 0 4,grow");
+		}
+
+	}
+
+	private void enableButtons(String[] strings) {
+		for(String skupina:components.keySet())
+		{
+			for(JComponent comp:components.get(skupina))
+			{
+				comp.setEnabled(false);
+			}
+		}
+		for(String skupina:strings)
+		{
+			for(JComponent comp:components.get(skupina))
+			{
+				comp.setEnabled(true);
+			}
+		}
+
 	}
 }
