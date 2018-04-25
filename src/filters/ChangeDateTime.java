34ebf2bbcb679e3bf171f5313019a374d7b1997c
JanezRadescek <janez.radescek@gmail.com>
2018-04-25 13:59:16 +0200

    save
    merge unfinished

---
+++ src/filters/ChangeDateTime.java
@@ -0,0 +1,203 @@
+package filters;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+/**
+ * Date in meta can only be set backwards not forwards as not to get negative timestamps
+ * if parseMeta is called without needed data we will get errors.
+ * @author janez
+ *
+ */
+public class ChangeDateTime extends Filter {
+
+	String dateM1 = null;
+	String timeM1 = null;
+	String zoneM1 = null;
+	String dateM2 = null;
+	String timeM2 = null;
+	String zoneM2 = null;
+
+	long posibleCorection = 0;
+	private long corection = 0;
+	boolean pushMeta;
+
+	public ChangeDateTime()
+	{
+	}
+	
+	public ChangeDateTime(long corection)
+	{
+		this.posibleCorection = corection;
+	}
+
+	public ChangeDateTime(String date, String time, String zone, boolean pushMeta) {
+		dateM2 = date;
+		timeM2 = time;
+		zoneM2 = zone;
+		this.pushMeta = pushMeta;
+	}
+
+	
+	
+	
+	/*
+	 * DOES NOT RETURN DESCENDATS RESULTS!
+	 * (non-Javadoc)
+	 * @see filters.Filter#onMetadata(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public boolean onMetadata(String key, String value) {
+
+		switch(key)
+		{
+		case "date" :
+			dateM1 = value;
+			break;
+		case "time" :
+			timeM1 = value;
+			break;
+		case "timezone" :
+			zoneM1 = value;
+			break;
+		default:
+			pushMetadata(key, value);
+			return true;
+		}
+
+		if(pushMeta && dateM1 != null && timeM1 == null && zoneM1 !=null)
+		{
+			parseMeta();
+		}
+		return true;
+	}
+
+
+	/**
+	 * Called when we have read metadata with time and date.
+	 * calculate time diference betwen files and set first date and time for new S2
+	 */
+	void parseMeta()
+	{
+		ZonedDateTime date1;
+		ZonedDateTime date2;
+		try
+		{
+			date1 = ZonedDateTime.parse(dateM1+"T"+timeM1+zoneM1);
+
+		}
+		catch(java.time.format.DateTimeParseException e)
+		{
+			zoneM1 = zoneM1.substring(0, 3) + ":" + zoneM1.substring(3, zoneM1.length());
+			date1 = ZonedDateTime.parse(dateM1+"T"+timeM1+zoneM1);
+		}
+
+
+		if(dateM2 !=null)
+		{
+			try
+			{
+				date2 = ZonedDateTime.parse(dateM2+"T"+timeM2+zoneM2);
+			}
+			catch(java.time.format.DateTimeParseException e)
+			{
+				zoneM2 = zoneM2.substring(0, 3) + ":" + zoneM2.substring(3, zoneM2.length());
+				date2 = ZonedDateTime.parse(dateM2+"T"+timeM2+zoneM2);
+			}
+
+			Duration difference = Duration.between(date1, date2);
+			//če je pozitivno je prvi datum prvi
+			posibleCorection = (long) (difference.getSeconds()*1E9) + difference.getNano();
+		}else
+		{
+
+			date2 = date1.plusNanos(posibleCorection);
+			dateM2 = date2.getYear()+"-"+date2.getMonthValue()+"-"+date2.getDayOfMonth();
+			timeM2 = date2.getHour()+":"+date2.getMinute()+":"+date2.getSecond()+"."+((int)(date2.getNano()/1E6));
+			zoneM2 = date2.getOffset().getId().substring(0, 6);
+		}
+
+		if(posibleCorection>0)
+		{
+			if(pushMeta)
+			{
+				pushMetadata("date", dateM1);
+				pushMetadata("time", timeM1);
+				pushMetadata("timezone", zoneM1);
+			}
+			corection = 0;
+		}else
+		{
+			if(pushMeta)
+			{
+				pushMetadata("date", dateM2);
+				pushMetadata("time", timeM2);
+				pushMetadata("timezone", zoneM2);
+			}
+			corection = posibleCorection;
+		}
+
+	}
+
+	@Override
+	public boolean onTimestamp(long nanoSecondTimestamp) {
+		return super.onTimestamp(nanoSecondTimestamp-corection);
+	}
+
+	@Override
+	public boolean onStreamPacket(byte handle, long timestamp, int len, byte[] data) {
+		return super.onStreamPacket(handle, timestamp-corection, len, data);
+	}
+	
+	
+	// NEW METHODS
+	
+	
+	void pushNewMeta()
+	{
+		pushMetadata("date", dateM2);
+		pushMetadata("time", timeM2);
+		pushMetadata("timezone", zoneM2);
+	}
+	
+	
+	//GETTERS SETTERS
+	
+	
+	public long getPosibleCorection()
+	{
+		return posibleCorection;
+	}
+	
+	
+	/**
+	 * @return 0 if we will corent this meta or posiblecorection if posiblecorection is >0 and thefore we didnt chage meta here.
+	 */
+	public long getNeededCorection()
+	{
+		return Math.max(0, posibleCorection);
+	}
+	
+	public void setPosibleCorection(long c)
+	{
+		posibleCorection = c;
+	}
+	
+	public void setPosible(long posibleCorection)
+	{
+		this.posibleCorection = posibleCorection;
+	}
+	
+	public void setMetaM2(String date, String time, String zone)
+	{
+		dateM2 = date;
+		timeM2 = time;
+		zoneM2 = zone;
+	}
+	
+	public void setPushMeta(boolean pushMeta)
+	{
+		this.pushMeta = pushMeta;
+	}
+
+}
--- src/filters/FilterMerge.java
+++ src/filters/FilterMerge.java
@@ -1,30 +1,56 @@
 package filters;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import si.ijs.e6.S2;
 import si.ijs.e6.S2.LoadStatus;
 
+/**
+ * 
+ * add merge as a child only to primary pipeline and provide start and end of secondary pipeline
+ * @author janez
+ *
+ */
 public class FilterMerge extends Filter {
 
+	//TODO unfinished class 
+	
 	//stuff for second S2 file
 	private LoadStatus ls;
 	private Filter firstFilter;
-	private Filter middleFilter;
+	//COMON
+	Pipe exitFilter;
 	//filters for regulating second S2
-	FilterTime ft;
-	FilterCounter fc;
-	FilterGetVersion fv;
-	Pipe lastFilter;
+	ChangeDateTime cdtS;
+	FilterTime ftS;
+	FilterGetVersion fvS;
+	//PRIMARY PIPELINES
+	ChangeDateTime cdtP;
 
 	//stuff we need to know for some time
+	long lastTimeP = 0;
 
+
+	//CONSTANTS
+	ArrayList<String> specialMeta = new ArrayList<String>();
+	Map<String,String> metaP = new HashMap<String,String>();
+
+	/**
+	 * Stuff we need to do on construction
+	 */
+	private FilterMerge()
+	{
+		specialMeta.add("date");
+		specialMeta.add("time");
+		specialMeta.add("timezone");
+	}
 
 	public FilterMerge(LoadStatus ls) 
 	{
 		this(ls, new Pipe());
 	}
-	
+
 	/**
 	 * @param ls load status of second file
 	 * @param firstFilter 
@@ -33,7 +59,7 @@ public class FilterMerge extends Filter {
 	{
 		this(ls, firstFilter,firstFilter);
 	}
-	
+
 	/**
 	 * @param ls load status of second file
 	 * @param firstFilter
@@ -41,28 +67,37 @@ public class FilterMerge extends Filter {
 	 */
 	public FilterMerge(LoadStatus ls,Filter firstFilter, Filter middleFilter) 
 	{
+		this();
 		this.ls = ls;
 		this.firstFilter = firstFilter;
-		this.middleFilter = middleFilter;
 
-		ft = new FilterTime(0, 0);
-		fc = new FilterCounter();
-		fv = new FilterGetVersion();
-		lastFilter = new Pipe();
+		//COMON EXIT
+		exitFilter = new Pipe();
 		
-		middleFilter.addChild(ft);
-		ft.addChild(fc);
-		fc.addChild(fv);
-		fv.addAncestor(lastFilter);
-		
+		//SECONDARY PIPES
+		cdtS = new ChangeDateTime();
+		ftS = new FilterTime(0, lastTimeP);
+		fvS = new FilterGetVersion();
+
+		middleFilter.addChild(cdtS);
+		cdtS.addChild(ftS);
+		ftS.addChild(fvS);
+		fvS.addChild(exitFilter);
+
+		//PRIMARY PIPES AFTER MERGE
+		cdtP = new ChangeDateTime();
+
+		children.add(cdtP);
+		cdtP.addChild(exitFilter);
+
 
 
 	}
 
 	@Override
 	public Filter addChild(Filter f) {
-		lastFilter.addChild(f);
-		return super.addChild(f);
+		exitFilter.addChild(f);
+		return f;
 	}
 
 
@@ -71,7 +106,7 @@ public class FilterMerge extends Filter {
 
 		ls.readLines(firstFilter, true);
 
-		if(versionInt == fv.versionInt && version.equals(fv.version))
+		if(versionInt == fvS.versionInt && version.equals(fvS.version))
 		{
 			return true;
 		}
@@ -79,9 +114,40 @@ public class FilterMerge extends Filter {
 		{
 			return false;
 		}
-		
 	}
 
+	@Override
+	public boolean onMetadata(String key, String value) {
+		if(!specialMeta.contains(key))
+		{
+			return super.onMetadata(key, value);
+		}
+		else
+		{
+			metaP.put(key, value);
+
+			if(metaP.size() == 3)
+			{
+				cdtS.setMetaM2(metaP.get("date"), metaP.get("time"), metaP.get("timezone"));
+				cdtS.parseMeta();
+				if(cdtS.getNeededCorection() != 0)
+				{
+					cdtP.setPosibleCorection(-cdtS.getNeededCorection());
+					cdtP.parseMeta();
+					cdtP.pushNewMeta();
+				}
+				else
+				{
+					//we changed secondary meta therefore this one stays the same
+					cdtS.pushNewMeta();
+				}
+			}
+			return true;
+		}
+	}
+	
+	
+	
 
 
 
